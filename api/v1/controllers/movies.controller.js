e013312a26e0db0deff70a6f4436443b9f165506
SimonDMaxwell <lolicongress@outlook.com>
2022-11-07 13:49:11 -0500

    tests

--- api/v1/controllers/movies.controller.js
+++ api/v1/controllers/movies.controller.js
@@ -77,6 +77,7 @@ class moviesController {
 		if (manageupload) body.clip = now + '_clip'
 		let newdoc = new movieModel(body)
 		let valid = true
+		console.log(err.errors[Object.keys(err.errors)[Object.keys(err.errors).length - 1]])
 		await newdoc.validate().catch((err) => {
 			valid = false
 			JSONResponse.error(
