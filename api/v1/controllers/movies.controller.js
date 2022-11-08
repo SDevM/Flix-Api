67926a32c47adb6079105a80d334c228f102d9de
SimonDMaxwell <lolicongress@outlook.com>
2022-11-08 08:41:44 -0500

    added missing await

--- api/v1/controllers/movies.controller.js
+++ api/v1/controllers/movies.controller.js
@@ -32,9 +32,7 @@ class moviesController {
 				JSONResponse.error(req, res, 500, 'Database Error', err)
 			})
 			if (bucket) {
-				bucket.populate('bucket', (records) => {
-					console.log(records)
-				})
+				await bucket.populate('bucket')
 				console.log(bucket)
 				let subArray = bucket.bucket.slice(indexStart, indexStart + limit)
 				JSONResponse.success(req, res, 200, 'Collected matching documents', subArray)
