b625efa3c2e7830b16afab7bf7fcf27260d36086
SimonDMaxwell <lolicongress@outlook.com>
2022-11-08 10:12:30 -0500

    test

--- api/v1/hub.js
+++ api/v1/hub.js
@@ -106,7 +106,7 @@ router.route('/s3/:key').get(async (req, res) => {
 		JSONResponse.error(req, res, 500, 'Failed to communicate with file storage')
 	})
 	let responseFile = await bufferToStream(file.buffer)
-	console.log(responseFile)
+	console.log(file.buffer)
 	if (file) responseFile.pipe(res)
 	else JSONResponse.error(req, res, 404, 'File not found')
 })
